e4d83e8e571cc987316e7fda93949db0a8a1e251
tahtaadi-np74 <77089190+tahtaadi-np74@users.noreply.github.com>
2023-08-30 15:45:44 +0800

    fix: dark theme display white text on input field

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,15 +9,15 @@ function ContactForm() {
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="block text-black text-md mb-2">Name</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="block border border-slate-500 rounded indent-3 h-10 w-full" />
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="block border border-slate-500 rounded indent-3 text-black h-10 w-full" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="block text-black text-md mb-2">Email</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="block border border-slate-500 rounded indent-3 h-10 w-full" />
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="block border border-slate-500 rounded indent-3 text-black h-10 w-full" />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="message" className="block text-black text-md mb-2">Message</label>
-                    <textarea id="message" name="message" value={formData.message} onChange={handleChange} className="block border border-slate-500 rounded p-3 w-full" cols="30" rows="10"></textarea>
+                    <textarea id="message" name="message" value={formData.message} onChange={handleChange} className="block border border-slate-500 rounded text-black p-3 w-full" cols="30" rows="10"></textarea>
                 </div>
                 <button type="submit" className="block bg-slate-800 text-white rounded ml-auto px-8 py-2">Send</button>
             </form>
